151c3dadaf3999b6490f416e0821e7cf2cc32c8d
memory-agape <memory.style.serv@gmail.com>
2019-06-28 13:12:09 +0900

    Update

--- src/app/containers/Login.tsx
+++ src/app/containers/Login.tsx
@@ -2,13 +2,16 @@ import * as React from 'react';
 const { useState } = React;
 import { Props, State } from '../ReactInterface';
 import '../style/components/login.scss';
+import '../style/components/input.scss';
 import '../style/containers/container.scss';
 
 export const Login = () => {
   return (
     <div className="container container__login">
       <div>
-        <div className="c-login-icon"></div>
+        <div className="text-center"><div className="c-login-icon"></div></div>
+        <p><input className="c-login-input" placeholder="ENTER YOUR ID" /></p>
+        <p><input className="c-login-input" placeholder="ENTER YOUR PASSWORD" /></p>
       </div>
     </div>
   );
