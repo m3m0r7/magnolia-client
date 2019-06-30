2da416ef797fe136fff8772199ba9507beaa7a82
memory-agape <memory.style.serv@gmail.com>
2019-06-30 21:53:08 +0900

    Update

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'
-import { compose, createStore } from 'redux'
+import { Provider } from 'react-redux';
+import { compose, createStore } from 'redux';
 
-import { Route, Switch } from 'react-router'
-import { ConnectedRouter } from 'connected-react-router'
+import { Route, Switch } from 'react-router';
+import { ConnectedRouter } from 'connected-react-router';
+
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
 
 // Styles
 import 'normalize.css';
@@ -17,28 +20,41 @@ import { NotFound } from "./app/containers/NotFound";
 // Stores
 import configureStore, { history, store } from './app/stores/configureStore';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#ef6c00',
+    },
+    secondary: {
+      main: '#f50057',
+    },
+  },
+});
+
 ReactDOM.render(
-  <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <>
-        <Switch>
-          <Route
-            exact
-            path="/(histories|favorites|settings|)?"
-            render={
-              (match: any) => {
-                return (
-                  <App
-                    match={match.match}
-                  />
-                );
+  <ThemeProvider theme={theme}>
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <>
+          <Switch>
+            <Route
+              exact
+              path="/(histories|favorites|settings|)?"
+              render={
+                (match: any) => {
+                  return (
+                    <App
+                      match={match.match}
+                    />
+                  );
+                }
               }
-            }
-          />
-          <Route render={() => <NotFound />} />
-        </Switch>
-      </>
-    </ConnectedRouter>
-  </Provider>,
+            />
+            <Route render={() => <NotFound />} />
+          </Switch>
+        </>
+      </ConnectedRouter>
+    </Provider>
+  </ThemeProvider>,
   document.getElementById('app')
 );
