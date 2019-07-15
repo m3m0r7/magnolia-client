a51f3383a70721df7b5527993d9945d0cef89ed8
memory-agape <memory.style.serv@gmail.com>
2019-07-15 14:20:41 +0900

    Update

--- src/app/containers/screens/LiveScreen.tsx
+++ src/app/containers/screens/LiveScreen.tsx
@@ -6,11 +6,13 @@ import Typography from '@material-ui/core/Typography';
 import * as API from '@util/API';
 
 import * as LiveStreamingEnv from '@envs/LiveStreaming';
+import moment from 'moment';
 
 // Load styles
 import '@style/components/live.scss';
 import '@style/components/card.scss';
 import { useCookies } from "react-cookie";
+import Timeout = NodeJS.Timeout;
 
 const { useState, useRef, useEffect } = React;
 
@@ -19,20 +21,17 @@ interface Navigator {
 }
 declare let navigator: Navigator;
 
-const updateStaticImage = (): void => {
-  API
-    .call('/api/v1/capture')
-    .then((response: any) => {
-      return response.json()
-    })
-    .then((json: any) => {
-      console.log(json);
-    });
-};
+enum renderingTypeEnum {
+  LIVE,
+  STATIC,
+}
 
 export const LiveScreen = (props: any) => {
   const [ cookies, setCookie, removeCookie ] = useCookies();
   const canvasRef = useRef(null);
+  const imageRef = useRef(null);
+
+  const [ renderingType, setRenderingType ] = useState(renderingTypeEnum.STATIC);
 
   const selector = useSelector((state: any) => state);
 
@@ -45,41 +44,72 @@ export const LiveScreen = (props: any) => {
   }
 
   useEffect(() => {
-    const ctx = (canvasRef.current as any).getContext('2d');
 
     // High-speed internet
-    if (
-      !selector.setting.isEnabledLiveStreaming ||
-      navigator.connection.downlink > LiveStreamingEnv.HIGH_SPEED_INTERNET_BOUNDARY_VALUE
-    ) {
-      const ws = new WebSocket(API.camera());
-      const retryingConnection = (e: any) => {
-
-      };
-
-      ws.addEventListener('close', retryingConnection);
-      ws.addEventListener('error', retryingConnection);
-
-      ws.addEventListener('message', (e) => {
-        const image = new Image();
-        image.src = e.data;
-        ctx.drawImage(
-          image,
-          0,
-          0
-        );
-      });
-
-      return () => {
-        ws.close();
-      };
-    }
+    // if (
+    //   !selector.setting.isEnabledLiveStreaming ||
+    //   navigator.connection.downlink > LiveStreamingEnv.HIGH_SPEED_INTERNET_BOUNDARY_VALUE
+    // ) {
+    //   const ctx = (canvasRef.current as any).getContext('2d');
+    //   setRenderingType(renderingTypeEnum.LIVE);
+    //   const ws = new WebSocket(API.camera());
+    //   const retryingConnection = (e: any) => {
+    //
+    //   };
+    //
+    //   ws.addEventListener('close', retryingConnection);
+    //   ws.addEventListener('error', retryingConnection);
+    //
+    //   ws.addEventListener('message', (e) => {
+    //     const image = new Image();
+    //     image.src = e.data;
+    //     ctx.drawImage(
+    //       image,
+    //       0,
+    //       0
+    //     );
+    //   });
+    //
+    //   return () => {
+    //     ws.close();
+    //   };
+    // }
 
     // Slow internet (for cellular)
-    const getImage = setTimeout(() => {}, 0);
+
+
+    setRenderingType(renderingTypeEnum.STATIC);
+    let timeId: number | Timeout | undefined = undefined;
+
+    const updateStaticImage = (): void => {
+      API
+        .call('/api/v1/capture')
+        .then((response: any) => {
+          return response.json()
+        })
+        .then((json: any) => {
+          let remainingTimes = json.next_update - moment().unix();
+          if (remainingTimes < 0) {
+            remainingTimes = 0;
+          }
+
+            (imageRef.current as any).src = json.image;
+
+            timeId = setTimeout(
+            () => {
+              updateStaticImage();
+            },
+            remainingTimes
+          );
+        });
+    };
+
+    updateStaticImage();
 
     return () => {
-
+      if (timeId !== undefined) {
+        clearTimeout(timeId as number);
+      }
     };
   });
 
@@ -87,9 +117,14 @@ export const LiveScreen = (props: any) => {
     <>
       <div className="c-live-image-container">
         <div className="c-live-image">
-          <canvas ref={canvasRef} width="600" height="450">
-
-          </canvas>
+          {
+            renderingType == renderingTypeEnum.LIVE &&
+              <canvas ref={canvasRef} width="600" height="450" />
+          }
+          {
+            renderingType == renderingTypeEnum.STATIC &&
+              <img ref={imageRef} width="600" height="450" alt="" />
+          }
         </div>
       </div>
     </>
